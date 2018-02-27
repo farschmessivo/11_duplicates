1eecac5dc039f74ae1756eaf85e200a493b5113f
Roman Ch <hello@romangagarin.com>
2018-02-27 23:53:51 +0000

    change

--- dups_27feb.py
+++ dups_27feb.py
@@ -8,7 +8,7 @@ def find_duplicates(directory):
     for root, dirs, files in os.walk(directory):
         for filename in files:
             file_path = os.path.join(root, filename)
-            file_hash = hashfile(file_path)
+            file_hash = get_file_hash(file_path)
             if file_hash in duplicates:
                 duplicates[file_hash].append(file_path)
             else:
@@ -16,21 +16,13 @@ def find_duplicates(directory):
     return duplicates
 
 
-def join_dictionaries(dict1, dict2):
-    for key in dict2.keys():
-        if key in dict1:
-            dict1[key] = dict1[key] + dict2[key]
-        else:
-            dict1[key] = dict2[key]
-
-
-def hashfile(path, blocksize=4096):
+def get_file_hash(path, blocksize=4096):
     with open(path, 'rb') as filename:
         hasher = hashlib.md5()
         buffer = filename.read(blocksize)
         while len(buffer) > 0:
-            hasher.update(buffer)
             buffer = filename.read(blocksize)
+            hasher.update(buffer)
         return hasher.hexdigest()
 
 
@@ -52,10 +44,9 @@ def print_results(dict1):
 
 if __name__ == '__main__':
     if sys.argv:
-        duplicates = {}
         root = sys.argv[1]
         print('Scanning... {}'.format(root))
-        join_dictionaries(duplicates, find_duplicates(root))
+        duplicates = find_duplicates(root)
         print_results(duplicates)
     else:
         sys.exit('Usage: python3 duplicates.py folder')
